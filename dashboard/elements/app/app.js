cd749b4d3bb05fcace88da36a8e129fa84169b0d
Don Maxito <mh@expertsinside.com>
2018-02-04 15:12:18 -0600

    working on dashboard logic

--- dashboard/elements/app/app.js
+++ dashboard/elements/app/app.js
@@ -1,16 +1,13 @@
 import XAppScreen from '/elements/x-screen/x-app-screen.js';
-import ScreenDashboard from '../screen-select-action/screen-select-action.js';
 import ScreenLoading from '/elements/screen-loading/screen-loading.js';
 import ScreenOverview from '../screen-overview/screen-overview.js';
-import ScreenSelectAction from '../screen-select-action/screen-select-action.js';
-import ActivationApi from '/elements/x-activation-api/x-activation-api.js'
+import ActivationTools from '/elements/x-activation-api/x-activation-api.js'
 
 export default class Dashboard extends XAppScreen {
     html() {
         return `
             <h1>Accounts Dashboard</h1>
             <screen-loading></screen-loading>
-            <screen-select-action></screen-select-action>
             <screen-overview></screen-overview>
             <x-activation-api></x-activation-api>
         `
@@ -18,19 +15,25 @@ export default class Dashboard extends XAppScreen {
 
     children() {
         return [
-            ScreenLoading, ScreenSelectAction, ScreenOverview, ActivationApi
+            ScreenLoading, ScreenOverview, ActivationApi
         ]
     }
 
     listeners() {
         return {
             'x-api-ready': '_onApiReady',
+            'x-api-dashboard-data': '_onDashboardDataResult',
         }
     }
 
     onEntry() {
-        // api.getAddressList({dashboard_token})
+        const dashboard_token = null; // Todo get token from url
+        this.$activationApi.getAddressList({dashboard_token})
+    }
 
+    _onDashboardDataResult(e) {
+        const { addresses, activationToken } = e.details;
+        this.$screenOverview.set({ addresses, activationToken });
     }
     
     _getDefaultScreen() { return this.$screenLoading; }

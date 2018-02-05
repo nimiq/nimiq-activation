6c5b53569c1d789934d3ec17035714239f05e68c
robinlinus <robinlinus@users.noreply.github.com>
2018-02-05 13:15:46 -0600

    Add screen-error; delete screen-overview

--- dashboard/elements/app/app.js
+++ dashboard/elements/app/app.js
@@ -1,23 +1,25 @@
 import XAppScreen from '/elements/x-screen/x-app-screen.js';
 import ScreenLoading from '/elements/screen-loading/screen-loading.js';
-import ScreenOverview from '../screen-overview/screen-overview.js';
-import ActivationUtils from '/elements/x-activation-utils/x-activation-utils.js'
+import ScreenError from '/elements/screen-error/screen-error.js';
+import ScreenAccounts from '/elements/screen-accounts/screen-accounts.js';
+import XActivationUtils from '/elements/x-activation-utils/x-activation-utils.js'
 
 export default class Dashboard extends XAppScreen {
     html() {
         return `
-            <h1>Accounts Dashboard</h1>
-            <screen-loading></screen-loading>
-            <screen-overview></screen-overview>
-            <x-activation-utils></x-activation-utils>
+            <div class="x-screen" style="display:flex;">
+                <h1>Accounts Dashboard</h1>
+                <x-slides>
+                    <screen-loading>Loading Data</screen-loading>                
+                    <screen-error>Loading Accounts</screen-error>                
+                    <screen-accounts></screen-accounts>
+                </x-slides>
+                <x-activation-utils></x-activation-utils>
+            </div>
         `
     }
 
-    children() {
-        return [
-            ScreenLoading, ScreenOverview, ActivationUtils
-        ]
-    }
+    children() { return [ ScreenLoading, ScreenAccounts, ScreenError, XActivationUtils ] }
 
     listeners() {
         return {
@@ -28,17 +30,14 @@ export default class Dashboard extends XAppScreen {
     }
 
     _onEntry() {
-        const dashboard_token = new URLSearchParams(document.location.search).get("dashboard_token")
+        const dashboard_token = new URLSearchParams(document.location.search).get("dashboard_token");
         this.$activationUtils.getDashboardData(dashboard_token);
     }
 
-    _getDefaultScreen() { return this.$screenLoading; }
-
     _onDashboardDataResult(e) {
-        const { addresses, activationToken } = e.details;
+        const { addresses, activationToken } = e.detail;
         this._activationToken = activationToken;
         this.$screenOverview.addresses = addresses;
-        this.goTo('overview');
     }
 
     _onCreateAccount() {

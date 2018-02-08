d3bbaefe5367b96b57928644f0ebd6042d44e471
Don Maxito <mh@expertsinside.com>
2018-02-08 14:21:15 -0600

    verifyfy: added fieldsets and legends to kyc form

--- verify/elements/screen-form-handler/screen-form/screen-form.js
+++ verify/elements/screen-form-handler/screen-form/screen-form.js
@@ -7,37 +7,39 @@ export default class ScreenForm extends XScreen {
             <h1>Enter your Details</h1>
             <h2>Due to anti money laundering laws we need you to fill out the following form</h2>
             <form>
-                <div>
-                <label>Country of Residence</label>
-                <x-country-select name="country_residence" required></x-country-select>
-                </div>
+                <fieldset>
+                <legend>Please match exactly information of identifying document</legend>
                 <div>
                 <label>Nationality</label>
                 <x-country-select name="country_nationality" required></x-country-select>
-                </div>
-                <div>
-                <label>E-Mail Address</label>
-                <input name="email" maxlength="100" type="email" placeholder="satoshin@gmx.com" required/>
-                </div>
+                </div> 
                 <div>
                     <label for="gender">Title</label>
                     <select name="gender" required>
                         <option value="0">Mr.</option>
-                        <option value="1">Mrs.</option>
+                        <option value="1">Mrs./Ms.</option>
                     </select>
                 </div>
                 <div>
-                <label for="first_name">First name</label>
+                <label for="first_name">First name(s)</label>
                 <input name="first_name" maxlength="100" placeholder="Satoshi" required />
                 </div>
                 <div>
-                <label for="last_name">Last name</label>
+                <label for="last_name">Last name(s)</label>
                 <input name="last_name" maxlength="100" placeholder="Nakamoto" required />
                 </div>
                 <div>
-                <label>Birthday</label>
+                <label>Date of Birth</label>
                 <input name="birthday" placeholder="YYYY-MM-DD" pattern="(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))" required />
+                </div> 
+                </fieldset>
+                <fieldset>
+                    <legend>Please provide your current address of residence</legend>
+                <div>
+                <label>Country of Residence</label>
+                <x-country-select name="country_residence" required></x-country-select>
                 </div>
+                
                 <div>
                 <label>Address (Street, Flat)</label>
                 <input name="street" maxlength="100" required />
@@ -50,6 +52,15 @@ export default class ScreenForm extends XScreen {
                 <label>Zip Code</label>
                 <input name="zip" maxlength="100" required />
                 </div>
+                </fieldset>
+                <fieldset>
+                <legend>Please provide your personal email address, to which the NIM activation link
+                        will be sent after passing KYC/AML</legend>
+                <div>
+                <label>E-Mail</label>
+                <input name="email" maxlength="100" type="email" placeholder="satoshin@gmx.com" required/>
+                </div>
+                </fieldset>
 
                 <button type="submit">Send</button>
             </form>

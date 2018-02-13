e2b2ef06529ff662acd466270748bbb1e31c74cf
Don Maxito <maxharz@gmail.com>
2018-02-12 18:39:16 -0600

    verify: disagree click leads to nimiq.com

--- verify/elements/screen-terms/screen-terms.js
+++ verify/elements/screen-terms/screen-terms.js
@@ -19,7 +19,7 @@ export default class ScreenTerms extends XScreen {
 		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		    	</p>
 		    	<div class="same-size-buttons">
-				    <a id="disagree" button class="small fixed-size secondary">I Disagree</a>
+				    <a id="disagree" button class="small fixed-size secondary" href="https://www.nimiq.com">I Disagree</a>
 				    <a id="agree" button class="small fixed-size" href="#form-handler">I Agree</a>
 				</div>
 		    </section>

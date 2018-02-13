f5e08b94ac3cde4ef327ec2fb71881dc8745be15
Don Maxito <maxharz@gmail.com>
2018-02-12 20:31:36 -0600

    verify: check for iOS or safari and show error

--- verify/elements/app/app.js
+++ verify/elements/app/app.js
@@ -1,10 +1,10 @@
 import XAppScreen from '/elements/x-screen/x-app-screen.js';
-import XSlideIndicator from '/elements/x-slide-indicator/x-slide-indicator.js';
 import ScreenWelcome from '../screen-welcome/screen-welcome.js';
 import ScreenTerms from '../screen-terms/screen-terms.js';
 import ScreenFormHandler from '../screen-form-handler/screen-form-handler.js';
 import ScreenSuccess from '/elements/screen-success/screen-success.js';
 import ScreenProceed from '../screen-proceed/screen-proceed.js';
+import ScreenError from '/elements/screen-error/screen-error.js';
 
 export default class Verify extends XAppScreen {
     html() {
@@ -14,6 +14,7 @@ export default class Verify extends XAppScreen {
             <screen-form-handler></screen-form-handler>
             <screen-success>Thank you! Soon you will receive an email with further information.</screen-success>
             <screen-proceed></screen-proceed>
+            <screen-error message="Your browser is not supported by our external service providers. Sorry!"></screen-error>
         `
     }
 
@@ -29,6 +30,8 @@ export default class Verify extends XAppScreen {
         this.$screenSuccess = null;
         /** @tpye {ScreenProceed} */
         this.$screenProceed = null;
+        /** @type {ScreenError} */
+        this.$screenError = null;
     }
 
     children() {
@@ -37,8 +40,18 @@ export default class Verify extends XAppScreen {
             ScreenTerms,
             ScreenFormHandler,
             ScreenSuccess,
-            ScreenProceed
+            ScreenProceed,
+            [ ScreenError ]
         ]
+    }
+
+    _onEntry() {
+        const ua = navigator.userAgent.toLowerCase();
+        const iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+        const safari = ua.includes('safari') && !ua.includes('chrome');
+        if (iOS || safari) {
+            this.goTo('error');
+        }
     }
 }
 

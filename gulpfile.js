20d24876f43d2a74f2a22b68f8eb0c6a41a95431
danimoh <github.danimoh@neverbox.com>
2018-02-15 21:40:23 -0600

    remove /dist and support minification of js in build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp');
+
 const clean = require('gulp-clean');
 const sourcemaps = require('gulp-sourcemaps');
+
 const rollup = require('gulp-better-rollup');
 const rollupRoot = require('rollup-plugin-root-import');
+const babel = require('gulp-babel');
+
 const cssImport = require('gulp-cssimport');
 const cleanCss = require('gulp-clean-css');
+
 const replace = require('gulp-replace');
 const htmlReplace = require('gulp-html-replace');
+const rename = require('gulp-rename');
+const clone = require('gulp-clone');
 const merge = require('merge2');
 
 const ROOT_PATH = `${__dirname}/../../`;
@@ -28,23 +35,16 @@ function bundleJs(appName) {
 }
 
 function bundleCss(appName) {
-    //const proccessed = new Set();
     return gulp.src(`${appName}/elements/app/app.css`)
         .pipe(sourcemaps.init())
         .pipe(cssImport({
             includePaths: [ROOT_PATH],
-            transform: path => {
-                // transform absolute paths to nimiq root path
-                if (path.startsWith('/')) path = `${__dirname}/../..${path}`;
-                /*if (proccessed.has(path)) return false; // works but leaves the @import
-                proccessed.add(path); */
-                return path;
-            }
+            transform: path => path.startsWith('/')? `${__dirname}/../..${path}` : path
         }))
         // the css import will inline the same css multiple times if imported multiple times thus we'll clean it up.
         .pipe(cleanCss({
             level: 2
-        }))
+        }));
 }
 
 function moveAssets(assetPaths, jsStream, cssStream) {
@@ -63,9 +63,10 @@ function moveAssets(assetPaths, jsStream, cssStream) {
 function getAssets(appName) {
     switch (appName) {
         case 'verify':
-        case 'activate':
         case 'dashboard':
             return ['/elements/screen-error/screen-error.svg'];
+        case 'activate':
+            return ['/elements/screen-error/screen-error.svg', '/libraries/qr-scanner/qr-scanner-worker.min.js'];
         default:
             return [];
     }
@@ -76,12 +77,19 @@ function build(appName) {
     let cssStream = bundleCss(appName);
     let assetsStream;
     [jsStream, cssStream, assetsStream] = moveAssets(getAssets(appName), jsStream, cssStream);
+    const minJsStream = jsStream
+        .pipe(clone())
+        .pipe(rename('app.min.js'))
+        .pipe(babel({
+            presets: ['minify']
+        }));
     const htmlStream = gulp.src(`${appName}/index.html`)
         .pipe(htmlReplace({
             'js': 'app.js',
             'css': 'app.css'
         }));
-    return merge([jsStream, cssStream, assetsStream, htmlStream])
+    return merge([jsStream, minJsStream, cssStream, assetsStream, htmlStream])
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(`${appName}/dist/`));
 }
 
@@ -104,5 +112,3 @@ gulp.task('build-dashboard-app', () => build('dashboard'));
 gulp.task('clean', ['clean-activate-app', 'clean-verify-app', 'clean-dashboard-app']);
 gulp.task('default', ['build-activate-app', 'build-dashboard-app', 'build-verify-app']);
 
-
-// TODO minification

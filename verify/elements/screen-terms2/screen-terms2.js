3a285dbe0c3d13a0764e487397a90ad942374ab0
sisou <hello@soerenschwert.de>
2018-02-15 20:21:34 -0600

    Improve styling of term&conditions screens

--- verify/elements/screen-terms2/screen-terms2.js
+++ verify/elements/screen-terms2/screen-terms2.js
@@ -1,22 +1,90 @@
-import XScreen from '/elements/x-screen/x-screen.js';
+import XScreenFit from '/elements/x-screen/x-screen-fit.js';
 
-export default class ScreenTerms2 extends XScreen {
+export default class ScreenTerms2 extends XScreenFit {
     html() {
         return `
 		    <h1>Privacy Policy</h1>
 		    <h2>Please read the following terms carefully</h2>
 		    <section>
-		    	<h3>Heading 1</h3>
+		    	<h3>Lorem ipsum dolor</h3>
 		    	<p>
-		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, id partem aliquip ancillae has, ius ut bonorum torquatos voluptatum. Ut minimum lobortis dissentias mel, dico signiferumque ex ius. Mazim denique neglegentur his id, sale molestie oportere eu vix. Ei ius fastidii salutandi, ad sea ferri corpora. Quando cetero sit eu, ut mel sale graece reprehendunt. Ipsum habemus in has.
 		    	</p>
-		    	<h3>Heading 2</h3>
+		    	<h3>An quo probo</h3>
 		    	<p>
-		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					An quo probo constituto interesset, integre cotidieque sea id. Mei id alia assueverit. Mel an sale cotidieque philosophia, vix nemore evertitur ex. Vis latine deleniti no. An ancillae abhorreant definitiones vim, no sint moderatius vis, eam ea munere sapientem. Ex tota graeco nec, sea ludus nominati ea.
 		    	</p>
-		    	<h3>Heading 3</h3>
+		    	<h3>Probo corpora</h3>
 		    	<p>
-		    		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Probo corpora mei eu, eam at reque voluptua. Sit cu ludus audire iuvaret, sed exerci offendit id, nibh scripta placerat eu has. Erant nostro has an, mea solet dissentiunt ut. Sit stet minimum et. Periculis interesset persequeris in nec. Omnes oportere eos et, autem invidunt periculis pro ex.
+		    	</p>
+		    	<h3>Sea eu everti</h3>
+		    	<p>
+					Sea eu everti discere gloriatur, graeco omnium cum id. Nam te maiorum pericula maluisset, vim aliquid lobortis eu. Usu ad modus iriure, discere commune liberavisse id vim, te eos expetenda expetendis. Verterem mnesarchum ea nam. Per in platonem molestiae, exerci ignota suscipiantur eos et, cum ea suas referrentur.
+		    	</p>
+		    	<h3>Doming timeam</h3>
+		    	<p>
+					Doming timeam instructior per in, at pro mollis assueverit. Usu debet partem ei. Erant feugiat constituam sit ad, an usu mazim nostrud antiopam. Ne suas possit referrentur sea, eum ex ignota malorum voluptatibus, nusquam vivendo ea duo. No cum facete nominavi abhorreant, mei ad wisi illud, vim cu novum molestiae. Graeci semper mea ex, delenit interpretaris signiferumque ius ne.
+		    	</p>
+		    	<h3>Id mei duis petentium</h3>
+		    	<p>
+					Id mei duis petentium, ea partem labore sea. Cu eum nullam putent adipisci. Vivendum mediocrem neglegentur has at. Stet nibh ullamcorper ei usu. Sit ponderum volutpat et. In est epicuri voluptua theophrastus.
+		    	</p>
+		    	<h3>Oblique propriae</h3>
+		    	<p>
+					Oblique propriae sententiae ne est, eam an vide feugiat. Et vel menandri maiestatis consequuntur. Alii sumo impetus id quo, ei erant causae mel, duo aperiam docendi an. At est prima soleat, vim erat meis malorum eu, eu qui nostrum convenire gloriatur. Ne senserit salutandi maluisset duo, duo id tritani appellantur. Et duo dico fierent. Ut agam nostrud appareat eos.
+		    	</p>
+		    	<h3>Usu oporteat intellegat</h3>
+		    	<p>
+					Usu oporteat intellegat id, vidisse meliore omittam in eos. Nec timeam latine ex. Ad nulla ancillae abhorreant ius, nec maiestatis constituto at. Id cum homero tritani dissentias. Sit at dolore oportere, tempor prompta vivendo ex quo.
+		    	</p>
+		    	<h3>Eam eu brute</h3>
+		    	<p>
+					Eam eu brute offendit adipisci, ei nobis diceret quo, ut nobis ornatus nec. Eos enim aliquando in, nibh molestie te pro. Ad mei cibo option accusamus, dicant nostro fierent eam ex. Pro dicit fierent mandamus ea. No quo purto eros electram, cu vix dolorum appetere. Eum te facer corrumpit, ius eu etiam iriure lucilius, recusabo deseruisse intellegebat ex per.
+		    	</p>
+		    	<h3>Possit mollis utroque</h3>
+		    	<p>
+		    		Possit mollis utroque ex vim, mei at mollis legendos convenire, maiorum fastidii nominati vel in. Cu brute molestie duo, scaevola vulputate signiferumque qui cu. Debet numquam posidonium ne mea, malis melius aperiri eu ius. Consequat eloquentiam in eam.
+		    	</p>
+		    	<h3>Lorem ipsum dolor</h3>
+		    	<p>
+					Lorem ipsum dolor sit amet, id partem aliquip ancillae has, ius ut bonorum torquatos voluptatum. Ut minimum lobortis dissentias mel, dico signiferumque ex ius. Mazim denique neglegentur his id, sale molestie oportere eu vix. Ei ius fastidii salutandi, ad sea ferri corpora. Quando cetero sit eu, ut mel sale graece reprehendunt. Ipsum habemus in has.
+		    	</p>
+		    	<h3>An quo probo</h3>
+		    	<p>
+					An quo probo constituto interesset, integre cotidieque sea id. Mei id alia assueverit. Mel an sale cotidieque philosophia, vix nemore evertitur ex. Vis latine deleniti no. An ancillae abhorreant definitiones vim, no sint moderatius vis, eam ea munere sapientem. Ex tota graeco nec, sea ludus nominati ea.
+		    	</p>
+		    	<h3>Probo corpora</h3>
+		    	<p>
+					Probo corpora mei eu, eam at reque voluptua. Sit cu ludus audire iuvaret, sed exerci offendit id, nibh scripta placerat eu has. Erant nostro has an, mea solet dissentiunt ut. Sit stet minimum et. Periculis interesset persequeris in nec. Omnes oportere eos et, autem invidunt periculis pro ex.
+		    	</p>
+		    	<h3>Sea eu everti</h3>
+		    	<p>
+					Sea eu everti discere gloriatur, graeco omnium cum id. Nam te maiorum pericula maluisset, vim aliquid lobortis eu. Usu ad modus iriure, discere commune liberavisse id vim, te eos expetenda expetendis. Verterem mnesarchum ea nam. Per in platonem molestiae, exerci ignota suscipiantur eos et, cum ea suas referrentur.
+		    	</p>
+		    	<h3>Doming timeam</h3>
+		    	<p>
+					Doming timeam instructior per in, at pro mollis assueverit. Usu debet partem ei. Erant feugiat constituam sit ad, an usu mazim nostrud antiopam. Ne suas possit referrentur sea, eum ex ignota malorum voluptatibus, nusquam vivendo ea duo. No cum facete nominavi abhorreant, mei ad wisi illud, vim cu novum molestiae. Graeci semper mea ex, delenit interpretaris signiferumque ius ne.
+		    	</p>
+		    	<h3>Id mei duis petentium</h3>
+		    	<p>
+					Id mei duis petentium, ea partem labore sea. Cu eum nullam putent adipisci. Vivendum mediocrem neglegentur has at. Stet nibh ullamcorper ei usu. Sit ponderum volutpat et. In est epicuri voluptua theophrastus.
+		    	</p>
+		    	<h3>Oblique propriae</h3>
+		    	<p>
+					Oblique propriae sententiae ne est, eam an vide feugiat. Et vel menandri maiestatis consequuntur. Alii sumo impetus id quo, ei erant causae mel, duo aperiam docendi an. At est prima soleat, vim erat meis malorum eu, eu qui nostrum convenire gloriatur. Ne senserit salutandi maluisset duo, duo id tritani appellantur. Et duo dico fierent. Ut agam nostrud appareat eos.
+		    	</p>
+		    	<h3>Usu oporteat intellegat</h3>
+		    	<p>
+					Usu oporteat intellegat id, vidisse meliore omittam in eos. Nec timeam latine ex. Ad nulla ancillae abhorreant ius, nec maiestatis constituto at. Id cum homero tritani dissentias. Sit at dolore oportere, tempor prompta vivendo ex quo.
+		    	</p>
+		    	<h3>Eam eu brute</h3>
+		    	<p>
+					Eam eu brute offendit adipisci, ei nobis diceret quo, ut nobis ornatus nec. Eos enim aliquando in, nibh molestie te pro. Ad mei cibo option accusamus, dicant nostro fierent eam ex. Pro dicit fierent mandamus ea. No quo purto eros electram, cu vix dolorum appetere. Eum te facer corrumpit, ius eu etiam iriure lucilius, recusabo deseruisse intellegebat ex per.
+		    	</p>
+		    	<h3>Possit mollis utroque</h3>
+		    	<p>
+		    		Possit mollis utroque ex vim, mei at mollis legendos convenire, maiorum fastidii nominati vel in. Cu brute molestie duo, scaevola vulputate signiferumque qui cu. Debet numquam posidonium ne mea, malis melius aperiri eu ius. Consequat eloquentiam in eam.
 		    	</p>
 		    	<div class="same-size-buttons">
 				    <a id="disagree" button class="small fixed-size secondary" href="https://www.nimiq.com">I Disagree</a>

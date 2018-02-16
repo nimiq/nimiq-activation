13bd90a73a9679f5592b3bd0825974c4c4f188a3
sisou <hello@soerenschwert.de>
2018-02-16 09:55:23 -0600

    Combine Term screens under one screen-terms

--- verify/elements/app/app.js
+++ verify/elements/app/app.js
@@ -1,7 +1,6 @@
 import XAppScreen from '/elements/x-screen/x-app-screen.js';
 import ScreenWelcome from '../screen-welcome/screen-welcome.js';
 import ScreenTerms from '../screen-terms/screen-terms.js';
-import ScreenTerms2 from '../screen-terms2/screen-terms2.js';
 import ScreenFormHandler from '../screen-form-handler/screen-form-handler.js';
 import ScreenSuccess from '/elements/screen-success/screen-success.js';
 import ScreenError from '/elements/screen-error/screen-error.js';
@@ -11,7 +10,6 @@ export default class Verify extends XAppScreen {
         return `
             <screen-welcome></screen-welcome>
             <screen-terms></screen-terms>
-            <screen-terms2></screen-terms2>
             <screen-form-handler></screen-form-handler>
             <screen-success>Thank you! Soon you will receive an email with further information.</screen-success>
             <screen-error route="kyc-error" message="Thank you! Soon you will receive an email with further information."></screen-error>
@@ -24,8 +22,6 @@ export default class Verify extends XAppScreen {
         this.$screenWelcome = null;
         /** @type {ScreenTerms} */
         this.$screenTerms = null;
-        /** @type {ScreenTerms2} */
-        this.$screenTerms2 = null;
         /** @type {ScreenForm} */
         this.$screenFormHandler = null;
         /** @type {ScreenSuccess} */
@@ -38,7 +34,6 @@ export default class Verify extends XAppScreen {
         return [
             ScreenWelcome,
             ScreenTerms,
-            ScreenTerms2,
             ScreenFormHandler,
             ScreenSuccess,
             ScreenError

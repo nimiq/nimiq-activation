9176413c6c76d6c68387fa7c9787bb4bc32acc57
danimoh <github.danimoh@neverbox.com>
2018-02-19 11:01:33 -0600

    country-check

--- verify/elements/app/app.js
+++ verify/elements/app/app.js
@@ -1,5 +1,6 @@
 import XAppScreen from '/elements/x-screen/x-app-screen.js';
 import ScreenWelcome from '../screen-welcome/screen-welcome.js';
+import ScreenCountryCheckFallback from '../screen-country-check-fallback/screen-country-check-fallback.js';
 import ScreenTerms from '../screen-terms/screen-terms.js';
 import ScreenFormHandler from '../screen-form-handler/screen-form-handler.js';
 import ScreenSuccess from '/elements/screen-success/screen-success.js';
@@ -9,10 +10,11 @@ export default class Verify extends XAppScreen {
     html() {
         return `
             <screen-welcome></screen-welcome>
+            <screen-country-check-fallback></screen-country-check-fallback>
             <screen-terms></screen-terms>
             <screen-form-handler></screen-form-handler>
             <screen-success>Thank you! Soon you will receive an email with further information.</screen-success>
-            <screen-error route="kyc-error" message="Thank you! Soon you will receive an email with further information."></screen-error>
+            <screen-error></screen-error>
         `
     }
 
@@ -33,6 +35,7 @@ export default class Verify extends XAppScreen {
     children() {
         return [
             ScreenWelcome,
+            ScreenCountryCheckFallback,
             ScreenTerms,
             ScreenFormHandler,
             ScreenSuccess,
@@ -42,6 +45,43 @@ export default class Verify extends XAppScreen {
 
     onCreate() {
         location.href = "#";
+        this._checkUserCountry();
+    }
+
+    _checkUserCountry() {
+        // check that user is not from the US or Puerto Rico
+        const xmlhttp = new XMLHttpRequest();
+        const url = 'https://geoip.nimiq-network.com:8443/v1/locate';
+        xmlhttp.onreadystatechange = () => {
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                const response = JSON.parse(xmlhttp.responseText);
+                let country;
+                try {
+                    country = response.country;
+                } catch(e) {
+                    country = null;
+                }
+                if (!country) {
+                    this._showCountryCheckFallback();
+                    return;
+                }
+                country = country.toUpperCase();
+                if (country==='US' || country==='PR') {
+                    // US or puerto rico
+                    this.showError('It appears as if you are accessing our site from the US or Puerto Rico. Please be aware that according to our KYC and Contribution Terms the NIM Activation is not available to citizens, green card holders or residents of the US and citizens or residents of Puerto Rico.');
+                }
+            }
+        };
+        xmlhttp.onerror = xmlhttp.ontimeout = () => {
+            this._showCountryCheckFallback();
+        };
+        xmlhttp.open('GET', url, true);
+        xmlhttp.timeout = 5000;
+        xmlhttp.send();
+    }
+
+    _showCountryCheckFallback() {
+        window.location = '#country-check-fallback'
     }
 }
 
---
+++ verify/elements/screen-country-check-fallback/screen-country-check-fallback.js
@@ -0,0 +1,23 @@
+import XScreen from '/elements/x-screen/x-screen.js';
+import Verify from '../app/app.js';
+
+export default class ScreenCountryCheckFallback extends XScreen {
+    html() {
+        return `
+            <nimiq-logo large>Nimiq Activation</nimiq-logo>
+            <h2>Are you a citizen, resident or green card holder of the United States or Puerto Rico?</h2>
+            <div buttons>
+                <a href="#" button button-yes>Yes</a><a button href="#terms">No</a>
+            </div>
+        `;
+    }
+
+    onCreate() {
+        this.$buttonYes = this.$('[button-yes]');
+        this.$buttonYes.addEventListener('click', () => this._showError());
+    }
+
+    _showError() {
+        Verify.instance.showError('Please be aware that according to our KYC and Contribution Terms the NIM Activation is not available to citizens, green card holders or residents of the US and citizens or residents of Puerto Rico.');
+    }
+}

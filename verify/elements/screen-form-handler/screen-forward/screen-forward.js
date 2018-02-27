05f51bf7f344bcb9f33df9634b4798ce4b681e62
danimoh <github.danimoh@neverbox.com>
2018-02-27 17:53:20 -0600

    Add a hint about which links in emails to trust

--- verify/elements/screen-form-handler/screen-forward/screen-forward.js
+++ verify/elements/screen-form-handler/screen-forward/screen-forward.js
@@ -6,8 +6,8 @@ export default class ScreenForward extends XScreen {
             <nimiq-logo large>Nimiq Activation</nimiq-logo>
             <h1>Data upload successful</h1>
             <h2>We are sending you an email with a link to start Identity Verification.</h2>
-            <p>Please check your inbox and also your spam folder for a message from us in the next few minutes.</p>
+            <p>Please check your inbox and also your spam folder for a message from us in the next few minutes. Please verify that the link we send you starts with https://go.online-ident.ch/... or https://nimiq.lon.netverify.com/...</p>
             <x-grow></x-grow>
-        `
+        `;
     }
 }

546926f1b33189ba391055aca2c0ecc23f9f36b0
sisou <hello@soerenschwert.de>
2018-04-03 17:24:55 -0600

    Remove word 'Genesis'

--- verify/elements/screen-welcome/screen-welcome.js
+++ verify/elements/screen-welcome/screen-welcome.js
@@ -5,7 +5,7 @@ export default class ScreenWelcome extends XScreen {
         return `
             <nimiq-logo large>Nimiq Activation</nimiq-logo>
             <h1>Welcome to Nimiq Activation</h1>
-            <h2>The Nimiq Activation Tool will help guide you through the process of activating your Genesis Nimiq (NIM) from NET.</h2>
+            <h2>The Nimiq Activation Tool will help guide you through the process of activating your Nimiq (NIM) from NET.</h2>
             <a button href="#terms">View Terms</a>
         `
     }
